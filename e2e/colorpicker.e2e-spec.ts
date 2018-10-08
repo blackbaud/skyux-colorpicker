92db6bc6273e04ce449d8fcd797c548bc20538fb
Conor Wright <Conor.Wright@blackbaud.com>
2018-10-08 15:56:15 -0400

    initial port of colorpicker

---
+++ e2e/colorpicker.e2e-spec.ts
@@ -0,0 +1,25 @@
+import {
+  element,
+  by
+} from 'protractor';
+
+import {
+  expect,
+  SkyHostBrowser
+} from '@skyux-sdk/e2e';
+
+describe('Colorpicker', () => {
+  beforeEach(() => {
+    SkyHostBrowser.get('visual/colorpicker');
+    SkyHostBrowser.setWindowBreakpoint('lg');
+  });
+
+  it('should match previous colorpicker screenshot', (done) => {
+    expect('#screenshot-colorpicker').toMatchBaselineScreenshot(done);
+  });
+
+  it('should match previous opened screenshot', (done) => {
+    element(by.css('sky-dropdown button')).click();
+    expect('#screenshot-colorpicker2').toMatchBaselineScreenshot(done);
+  });
+});
