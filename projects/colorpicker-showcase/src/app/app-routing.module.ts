7bd6d01a9fc6ebd9bb169ef9d02928f5b8ec9777
Paul Crowder <paul.crowder@blackbaud.com>
2021-08-23 11:31:18 -0400

    Next > Adopt Angular CLI (#96)
    * Migrate to Angular CLI
    
    * Fix karma conf
    
    * Fix code coverage
    
    * Fix peer dependency; configure screenshot branch
    
    * Delete old screenshots

---
+++ projects/colorpicker-showcase/src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { ColorpickerVisualComponent } from './visual/colorpicker/colorpicker-visual.component';
+
+const routes: Routes = [
+  {
+    path: 'visual/colorpicker',
+    component: ColorpickerVisualComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
