28c77a7445335f73d269d108b8f3366b28d8d236
Alex Kingman <alex.kingman@blackbaud.me>
2021-09-03 09:53:44 -0400

    exports

--- projects/colorpicker/src/public-api.ts
+++ projects/colorpicker/src/public-api.ts
@@ -9,3 +9,8 @@ export * from './modules/colorpicker/types/colorpicker-message-type';
 export * from './modules/colorpicker/types/colorpicker-output';
 export * from './modules/colorpicker/types/colorpicker-result';
 export * from './modules/colorpicker/types/colorpicker-rgba';
+
+// Components and directives must be exported to support Angular's "partial" Ivy compiler.
+// Obscure names are used to indicate types are not part of the public API.
+export { SkyColorpickerComponent as λ1 } from './modules/colorpicker/colorpicker.component';
+export { SkyColorpickerInputDirective as λ2 } from './modules/colorpicker/colorpicker-input.directive';
